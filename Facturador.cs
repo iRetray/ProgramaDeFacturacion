baa53dc8314a756d789121b240106a5761c7f9a1
Julian Camilo <julian20141016@gmail.com>
2020-10-30 16:05:08 -0500

    Initial commit

---
+++ Facturador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaDeFactoracion
+{
+    public class Facturador
+    {
+        List<Producto> listaProductos = new List<Producto>();
+
+        public void añadirProducto( Producto newProducto ){
+            listaProductos.Add(newProducto);
+        }
+
+        public List<Producto> obtenerProductos() {
+            return listaProductos;
+        }
+    }
+}
